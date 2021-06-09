34e94ed02e1338e870b41af95abb3e470d42c357
ikrammmmm <ikramhussein1234@gmail.com>
2021-06-10 01:45:35 +0530

    updated database.js

--- public/database.js
+++ public/database.js
@@ -1,12 +1,7 @@
 let database;
 const request = indexedDB.open("budget-tracker", 1);
 
-function saveRecord(transaction) 
-{
-  const transaction = database.transaction(["pending-transaction"], "readwrite");
-  const store = transaction.objectStore("pending-transaction");
-  store.add(transaction);
-}
+
 
 function checkLocalDB() {
   const transaction = database.transaction(["pending-transaction"], "readwrite");
@@ -31,7 +26,12 @@ function checkLocalDB() {
     }
   };
 }
-
+function saveRecord(transaction) 
+{
+  const transaction = database.transaction(["pending-transaction"], "readwrite");
+  const store = transaction.objectStore("pending-transaction");
+  store.add(transaction);
+}
 request.onupgradeneeded =  (event) =>{
   const database = event.target.result;
   database.createObjectStore("pending-transaction", { autoIncrement: true });
